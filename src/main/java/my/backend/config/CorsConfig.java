8bdfcd4066f21816a5337a3bef3709b857e5de28
zzzmini <sm01079200887@gmail.com>
2025-03-21 13:34:00 +0900

    cors s3 update

--- src/main/java/my/backend/config/CorsConfig.java
+++ src/main/java/my/backend/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://3.37.155.248") // React 서버 주소
+//                        .allowedOrigins("http://3.37.155.248") // React 서버 주소
+                        .allowedOrigins("http://zzzmini-react-app.s3-website.ap-northeast-2.amazonaws.com") // S3 URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
